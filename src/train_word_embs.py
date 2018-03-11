e56b85575c2690d788999d65a8e2299703264ffc
Serina Chang <sc3003@columbia.edu>
2018-03-10 22:36:30 -0500

    Train word embeddings on any data, performing the standard pre-processing from utils.

---
+++ src/train_word_embs.py
@@ -0,0 +1,48 @@
+import argparse
+from data_utils.utils import unicode_csv_reader2, parse_line, get_delimiter, datum_to_string, delete_files
+from gensim.models import Word2Vec
+
+EMB_WIDTH = 50
+MAX_VOCAB_SIZE = 20000
+
+def read_data(data_file):
+    tweets = []
+    delimiter = get_delimiter(data_file)
+    with open(data_file, 'r') as fhr:
+        reader = unicode_csv_reader2(fhr, encoding = 'utf8', delimiter = delimiter)
+        for row in reader:
+            X_c, _, _, _ = parse_line(row, text_column="text", label_column="label",
+                                      tweet_id_column="tweet_id", user_name_column="user_name",
+                                      max_len=100, normalize=True, word_level=True)
+            if X_c is not None:
+                tweets.append(X_c)
+    print tweets[:5]
+    return tweets
+
+def train_w2v(D):
+    print "Training word2vec on {} texts...".format(len(D))
+    model = Word2Vec(D, size=EMB_WIDTH, max_vocab_size=MAX_VOCAB_SIZE)
+    w2v = model.wv
+    print "Vocab size:", len(w2v.vocab)
+    return w2v
+
+def main(args):
+    data_file = args['data_file']
+    print "Reading data from {}...".format(data_file)
+    tweets = read_data(data_file)
+
+    save_file = args['save_file'] + ".h5"
+    w2v = train_w2v(tweets)
+    w2v.save_word2vec_format(save_file)
+    print "Saved w2v keyed vectors at", save_file
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = '')
+    parser.add_argument('-d', '--data_file', type = str, default = None, help = 'filename for raw texts')
+    parser.add_argument('-s', '--save_file', type = str, default = None, help = 'filename for saving w2v keyed vectors')
+
+    args = vars(parser.parse_args())
+
+    main(args)
+
+
